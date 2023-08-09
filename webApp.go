81b71c9188e52854c0dfc0732cd169bb7e042234
Sergey <hi.sergeyl@gmail.com>
2023-08-09 18:23:41 +0300

    add post(/task) & get(/tab)

--- webApp.go
+++ webApp.go
@@ -29,10 +29,50 @@ func Decrease(c *fiber.Ctx) error {
 	return c.SendString(strconv.Itoa(count))
 }
 
+func GetTab(c *fiber.Ctx) error {
+	return c.JSON(table)
+}
+
+var id int
+
+type Request struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+type Response struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	ID    int    `json:"id"`
+}
+
+var table = []Response{}
+
+func TaskReq(c *fiber.Ctx) error {
+	var req Request
+	if err := c.BodyParser(&req); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	id++
+	table = append(table, Response{
+		Name:  req.Name,
+		Email: req.Email,
+		ID:    id,
+	})
+	return c.Status(fiber.StatusOK).JSON(Response{
+		Name:  req.Name,
+		Email: req.Email,
+		ID:    id,
+	})
+}
+
 func main() {
 	webApp := fiber.New()
 	webApp.Get("/", Stat)
 	webApp.Get("/incr", Increase)
 	webApp.Get("/decr", Decrease)
+	webApp.Get("/tab", GetTab)
+
+	webApp.Post("/task", TaskReq)
+
 	logrus.Fatal(webApp.Listen(":80")) //localhost
 }
