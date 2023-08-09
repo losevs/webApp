b511784225b3c24f7096d29b7578525db5401ecd
Sergey <hi.sergeyl@gmail.com>
2023-08-09 17:13:42 +0300

    add status

--- webApp.go
+++ webApp.go
@@ -5,8 +5,12 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+func Stat(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).SendString("status ok")
+}
+
 func main() {
 	webApp := fiber.New()
-
-	logrus.Fatal(webApp.Listen(":80"))
+	webApp.Get("/status", Stat)
+	logrus.Fatal(webApp.Listen(":80")) //localhost
 }
